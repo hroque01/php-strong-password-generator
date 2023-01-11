c25e967e2be3e85f356dad8ae1b0c9a46750cb4b
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-01-11 19:14:59 +0100

    Create CSS of password generator

--- index.php
+++ index.php
@@ -2,28 +2,62 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
 </head>
 
+<style>
+    body {
+        background-color: #001732;
+    }
+
+    .grey {
+        color: #5f7c91;
+    }
+
+    .white {
+        color: white;
+    }
+
+    .white-bg {
+        background-color: white;
+    }
+
+    .cyan-bg {
+        background-color: #cef4fc;
+    }
+
+    .form {
+        margin-top: 20px;
+    }
+</style>
+
 <body>
     <?php require_once __DIR__ . '/helper.php';
     ?>
 
-    <!-- #### Milestone 1
-    Creare un `form` che invii in `GET` la lunghezza della password. Una nostra funzione utilizzerà *questo dato* per generare una **password casuale** da restituire all’utente. La **password** dovra' essere composta da *lettere minuscole e maiuscole, numeri e simboli*
-    Scriviamo tutto (logica e layout) in un unico file `index.php`. -->
+    <div class="container">
+        <h1 class="grey">Strong Password Generator</h1>
+        <h3 class="white">Genera password sicura</h3>
+        <div class="cyan-bg p-3">
+            <div>
+                <?php echo (@$result); ?>
+            </div>
+        </div>
 
-    <h1>
-        <?php echo (@$result); ?>
-    </h1>
-    <form method="get">
-        <label for="password">Lunghezza password</label>
-        <input type="number" name="passGen" id="passGen">
-        <input type="submit" value="Generate" name="gen">
-    </form>
+        <div class="white-bg form p-3">
+            <form method="get">
+                <label class="w-50" for="password">Lunghezza password</label>
+                <input type="text" name="passGen" id="passGen" placeholder="Scegli un numero">
+                <input type="submit" value="Generate" name="gen">
+            </form>
+        </div>
+
+    </div>
+
 </body>
 
 </html>
